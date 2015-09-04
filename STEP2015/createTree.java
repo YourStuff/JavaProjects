6c0da09c3d1e726f1d2f818eb229891487a91102
ismail Jaafari <iijaafari@gmail.com>
2015-09-04 09:39:17 -0400

    adding one more change

---
+++ STEP2015/createTree.java
@@ -0,0 +1,31 @@
+package STEP2015;
+
+import java.util.Scanner;
+
+public class createTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int i = 0;
+		 int j = 0;
+         int lenght = 10;
+         
+        
+         Scanner input = new Scanner(System.in);
+         lenght = input.nextInt();
+		    while (i < lenght) {
+		        
+		        i++;
+		        while(j<i){
+		        	System.out.print("*");
+			        j++;
+			        
+		        }
+		        System.out.println("");
+		        j =0;
+		    } 
+		       
+                
+}
+}
